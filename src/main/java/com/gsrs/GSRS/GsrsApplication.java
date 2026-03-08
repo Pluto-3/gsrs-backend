2122d05fee876db643c307716ef10a43875be544
Your Name <you@example.com>
2026-03-08 07:22:40 +0300

    Fix PageImpl serialization for Spring Boot 4

--- src/main/java/com/gsrs/GSRS/GsrsApplication.java
+++ src/main/java/com/gsrs/GSRS/GsrsApplication.java
@@ -2,8 +2,10 @@ package com.gsrs.GSRS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class GsrsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GsrsApplication.class, args);
